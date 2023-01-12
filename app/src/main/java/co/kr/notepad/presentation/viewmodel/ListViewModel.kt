5436b51165e2476cc820339c31c93366458ea0ad
hansh0101 <hansh0101@inha.edu>
2023-01-12 23:13:49 +0900

    [REFACTOR] #32 - Replace LiveData with StateFlow

--- app/src/main/java/co/kr/notepad/presentation/viewmodel/ListViewModel.kt
+++ app/src/main/java/co/kr/notepad/presentation/viewmodel/ListViewModel.kt
@@ -1,13 +1,13 @@
 package co.kr.notepad.presentation.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import co.kr.notepad.domain.entity.Memo
 import co.kr.notepad.domain.usecase.DeleteMemoUseCase
 import co.kr.notepad.domain.usecase.GetAllMemoUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
@@ -17,42 +17,57 @@ class ListViewModel @Inject constructor(
     private val getAllMemoUseCase: GetAllMemoUseCase,
     private val deleteMemoUseCase: DeleteMemoUseCase
 ) : ViewModel() {
-    private val _memos = MutableLiveData<List<Memo>>()
-    val memos: LiveData<List<Memo>> get() = _memos
     private val selectedList = mutableListOf<Memo>()
-    private val _selectedMemos = MutableLiveData<List<Memo>>(selectedList)
-    val selectedMemos: LiveData<List<Memo>> get() = _selectedMemos
+    private val _memos = MutableStateFlow<UiState<List<Memo>>>(UiState.Init)
+    val memos: StateFlow<UiState<List<Memo>>> get() = _memos
+    private val _selectedMemos = MutableStateFlow<UiState<List<Memo>>>(UiState.Init)
+    val selectedMemos: StateFlow<UiState<List<Memo>>> get() = _selectedMemos
 
     fun getAll() {
         viewModelScope.launch {
+            _memos.emit(UiState.Loading)
             getAllMemoUseCase()
-                .onSuccess { _memos.value = it }
-                .onFailure { Timber.e(it) }
+                .onSuccess {
+                    _memos.emit(UiState.Success(it))
+                }.onFailure {
+                    Timber.e(it)
+                    _memos.emit(UiState.Failure(it))
+                }
         }
     }
 
     fun delete() {
         viewModelScope.launch {
+            _selectedMemos.emit(UiState.Loading)
             deleteMemoUseCase(selectedList.toList())
                 .onSuccess {
                     getAll()
                     selectedList.clear()
-                    _selectedMemos.value = selectedList
-                }.onFailure { Timber.e(it) }
+                    _selectedMemos.emit(UiState.Success(selectedList))
+                }.onFailure {
+                    Timber.e(it)
+                    _selectedMemos.emit(UiState.Failure(it))
+                }
         }
     }
 
     fun updateSelectedMemos(memo: Memo) {
-        if (selectedList.contains(memo)) {
-            selectedList.remove(memo)
-        } else {
-            selectedList.add(memo)
+        _selectedMemos.value = UiState.Loading
+        selectedList.run {
+            if (this.contains(memo)) {
+                this.remove(memo)
+            } else {
+                this.add(memo)
+            }
+            _selectedMemos.value = UiState.Success(this)
         }
-        _selectedMemos.value = selectedList
     }
 
     fun clearSelectedMemos() {
-        selectedList.clear()
-        _selectedMemos.value = selectedList
+        _selectedMemos.value = UiState.Loading
+        selectedList.run {
+            this.clear()
+            _selectedMemos.value = UiState.Success(this)
+        }
     }
 }
--- app/src/main/java/co/kr/notepad/presentation/viewmodel/WriteViewModel.kt
+++ app/src/main/java/co/kr/notepad/presentation/viewmodel/WriteViewModel.kt
@@ -1,14 +1,14 @@
 package co.kr.notepad.presentation.viewmodel
 
 import android.net.Uri
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import co.kr.notepad.domain.entity.Memo
 import co.kr.notepad.domain.usecase.GetMemoUseCase
 import co.kr.notepad.domain.usecase.InsertOrUpdateMemoUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
@@ -18,11 +18,11 @@ class WriteViewModel @Inject constructor(
     private val insertOrUpdateMemoUseCase: InsertOrUpdateMemoUseCase,
     private val getMemoUseCase: GetMemoUseCase
 ) : ViewModel() {
-    private val _memo = MutableLiveData<Memo>()
-    val memo: LiveData<Memo> get() = _memo
-    private val _isErrorOccurred = MutableLiveData<Boolean>()
-    val isErrorOccurred: LiveData<Boolean> get() = _isErrorOccurred
-    val imageUri = MutableLiveData<Uri?>(null)
+    private val _memo = MutableStateFlow<UiState<Memo>>(UiState.Init)
+    val memo: StateFlow<UiState<Memo>> get() = _memo
+    private val _isSaved = MutableStateFlow<UiState<Unit>>(UiState.Init)
+    val isSaved: StateFlow<UiState<Unit>> get() = _isSaved
+    val imageUri = MutableStateFlow<Uri?>(null)
 
     fun insertOrUpdate(memoId: Long, title: String, text: String) {
         val newMemo =
@@ -33,10 +33,18 @@ class WriteViewModel @Inject constructor(
                 image = imageUri.value.toString(),
                 date = System.currentTimeMillis()
             )
-        if (title.isNotBlank() && !newMemo.isContentsTheSame(memo.value)) {
-            viewModelScope.launch {
-                insertOrUpdateMemoUseCase(newMemo)
-                    .onFailure { _isErrorOccurred.value = true }
+        if (memo.value is UiState.Success) {
+            if (title.isNotBlank() && !newMemo.isContentsTheSame((memo.value as UiState.Success).data)) {
+                viewModelScope.launch {
+                    _isSaved.emit(UiState.Loading)
+                    insertOrUpdateMemoUseCase(newMemo)
+                        .onSuccess {
+                            _isSaved.emit(UiState.Success(Unit))
+                        }.onFailure {
+                            Timber.e(it)
+                            _isSaved.emit(UiState.Failure(it))
+                        }
+                }
             }
         }
     }
@@ -46,11 +54,15 @@ class WriteViewModel @Inject constructor(
             return
         }
         viewModelScope.launch {
+            _memo.emit(UiState.Loading)
             getMemoUseCase(memoId)
                 .onSuccess { memoFromDatabase ->
-                    _memo.value = memoFromDatabase
-                    memoFromDatabase.image?.let { imageUri.value = Uri.parse(it) }
-                }.onFailure { Timber.e(it) }
+                    _memo.emit(UiState.Success(memoFromDatabase))
+                    memoFromDatabase.image?.let { imageUri.emit(Uri.parse(it)) }
+                }.onFailure {
+                    Timber.e(it)
+                    _memo.emit(UiState.Failure(it))
+                }
         }
     }
 }
